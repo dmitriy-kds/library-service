b76f71a4c38860453976b15b09424aa7526fc1e5
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-04-23 12:21:08 +0300

    feat: Implement book ViewSet and urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from books.models import Book
+from books.permissions import IsAdminOrIfUnauthenticatedReadOnly
+from books.serializers import BookSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrIfUnauthenticatedReadOnly]
+    authentication_classes = [JWTAuthentication]
