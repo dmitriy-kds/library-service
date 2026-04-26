46c9f79be1f53dbb77ed727dd888d02d13bea748
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-04-26 12:46:44 +0300

    chore: Move env variables to .env, update settings, create .env.sample

--- library_service/settings.py
+++ library_service/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-oc_sgxe8+4#ppgz=s=in@zv%=+1r=_e9%e=2i_rn^t3@)6n1w-"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
